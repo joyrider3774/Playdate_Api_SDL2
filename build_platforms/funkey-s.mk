be86ae5fd7dd647cf8653d488b3821675cbbc66e
willems davy <joyrider3774@hotmail.com>
2025-03-12 12:23:31 +0100

    add funkey-s.mk platform file

---
+++ build_platforms/funkey-s.mk
@@ -0,0 +1,10 @@
+
+TOOLCHAIN = /opt/funkey-sdk
+TOOLCHAIN_USR = /opt/funkey-sdk/arm-funkey-linux-musleabihf/sysroot/usr
+
+CC = $(TOOLCHAIN)/bin/arm-funkey-linux-musleabihf-gcc
+CPP = $(TOOLCHAIN)/bin/arm-funkey-linux-musleabihf-g++
+SDL2CONFIG = $(TOOLCHAIN_USR)/bin/sdl2-config
+LDUSEX11 = 0
+CFLAGS_EXTRA = -DFUNKEY
+LDFLAGS = -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx -lpthread
