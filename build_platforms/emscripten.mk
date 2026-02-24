97c839d6a2afaec40b404fa679b8ce3fdcb760b9
willems davy <joyrider3774@hotmail.com>
2026-02-24 22:14:41 +0100

    seems sdl2 gfx is still needed

--- build_platforms/emscripten.mk
+++ build_platforms/emscripten.mk
@@ -5,7 +5,7 @@ CC = emcc
 CPP = em++
 OPT_LEVEL = -O3
 #emscripten uses their own libs only ogg seemed to play fine browser
-CFLAGS = -Wno-error=implicit-function-declaration -sUSE_OGG=1 -sUSE_VORBIS=1 -sUSE_SDL=2 -sUSE_SDL_TTF=2 -sUSE_SDL_MIXER=2 -sUSE_SDL_IMAGE=2 -sSDL2_IMAGE_FORMATS='["png"]' -D_USE_MATH_DEFINES -DSDL2API -DTARGET_EXTENSION -Wall -Wextra -Wno-unused-parameter
+CFLAGS = -Wno-error=implicit-function-declaration -sUSE_OGG=1 -sUSE_VORBIS=1 -sUSE_SDL=2 -sUSE_SDL_TTF=2 -sUSE_SDL_GFX=2 -sUSE_SDL_MIXER=2 -sUSE_SDL_IMAGE=2 -sSDL2_IMAGE_FORMATS='["png"]' -D_USE_MATH_DEFINES -DSDL2API -DTARGET_EXTENSION -Wall -Wextra -Wno-unused-parameter
 #memory size for game i took about 500 MB but some games require more
 EMSCRIPTEN_MEMORY_SIZE=536870912
 #no X11 lib
