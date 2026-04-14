17695a0bc56c87b8a5536671ff1d407284085564
willems davy <joyrider3774@hotmail.com>
2026-04-14 18:34:19 +0200

    temporary enable debug info

--- src/srcstub/defines.h
+++ src/srcstub/defines.h
@@ -1,11 +1,11 @@
 #define MAXPATH 300             //max size of path (i used 300 but max is probably around 260 or so)
 #define ARRAY_LIST_INCSIZES 1   //dynamic arrays minimum list sizes and increases
 #define FORMAT_STRING_BUFLENGTH 10000
-#define DISABLEDEBUGMESSAGES 1
+#define DISABLEDEBUGMESSAGES 0
 #define DEBUGTRACEFUNCTIONS 0
 #define DEBUGNOTIMPLEMENTEDFUNCTIONS 0
 #define SHOWCLEARCOLOR 0
-#define DEBUGINFO 0
+#define DEBUGINFO 1
 #define DEBUGFPS 0
 #define DRAWBUMPRECTS 0
 #define DEBUG_JOYSTICK_BUTTONS 0
